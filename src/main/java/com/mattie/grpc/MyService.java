1a82571d5eb3fbc23a118d3ac4ffa4b9b566f4ce
Ted Wei <witwiky@me.com>
2018-09-16 23:14:46 +0800

    initial commit

--- src/main/java/com/mattie/grpc/MyService.java
+++ src/main/java/com/mattie/grpc/MyService.java
@@ -3,10 +3,12 @@ package com.mattie.grpc;
 import com.mattie.grpc.GreeterGrpc.GreeterImplBase;
 import io.grpc.stub.StreamObserver;
 
+import static com.mattie.grpc.HelloWorldProtos.*;
+
 public class MyService extends GreeterImplBase {
     @Override
-    public void sayHello(HelloWorldProtos.HelloRequest request, StreamObserver<HelloWorldProtos.HelloReply> responseObserver) {
-        HelloWorldProtos.HelloReply helloReply = HelloWorldProtos.HelloReply.newBuilder().setMessage("hello client.").build();
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply helloReply = HelloReply.newBuilder().setMessage("hello client.").build();
         responseObserver.onNext(helloReply);
         responseObserver.onCompleted();
     }
